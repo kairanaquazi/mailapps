962568f8e7cb0b098ad5aab034099a3a446d4718
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2019-04-26 20:54:33 -0700

    update for security

--- mailtest.py
+++ mailtest.py
@@ -4,19 +4,20 @@ import os
 
 app = Flask(__name__)
 
+
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME":'jeffthefreshavocado',
-    "MAIL_PASSWORD": 'chickennuggets'
+    "MAIL_USERNAME": <your email w/o the domain>,
+    "MAIL_PASSWORD": <your email password>
 }
 
 app.config.update(mail_settings)
 mail = Mail(app)
 
-print("whats yee email?")
+print("what's the email?")
 email=input()
 
 print("body")
@@ -24,7 +25,7 @@ message=input()
 if __name__ == '__main__':
     with app.app_context():
         msg = Message(subject="Verify Link",
-                      sender='jeffthefreshavocado@gmail.com',
-                      recipients=['jeffthefreshavocado@gmail.com',email],
+                      sender=<your email>,
+                      recipients=[email],
                       body=message)
         mail.send(msg)
